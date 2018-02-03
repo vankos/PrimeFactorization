fce2843d7ee1fdfb42aeae803afeffac26bb243f
vankos <vano111ru@yandex.ru>
2018-02-03 17:15:47 +0300

    Add project files.

---
+++ Prime Factorization/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Factorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
